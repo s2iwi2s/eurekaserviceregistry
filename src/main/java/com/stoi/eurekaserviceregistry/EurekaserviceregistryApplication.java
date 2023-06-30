5f26cd472177001a390f3861c3d3ffa5a1276fec
Winston Pidor <s2iwi2s@gmail.com>
2023-06-30 17:07:32 -0500

    first commit

--- src/main/java/com/stoi/eurekaserviceregistry/EurekaserviceregistryApplication.java
+++ src/main/java/com/stoi/eurekaserviceregistry/EurekaserviceregistryApplication.java
@@ -1,13 +1,60 @@
 package com.stoi.eurekaserviceregistry;
 
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaserviceregistryApplication {
+    private static final Logger log = LoggerFactory.getLogger(EurekaserviceregistryApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaserviceregistryApplication.class, args);
-	}
+    public static void main(String[] args) {
+//        SpringApplication.run(EurekaserviceregistryApplication.class, args);
 
+        SpringApplication app = new SpringApplication(EurekaserviceregistryApplication.class);
+        Environment env = app.run(args).getEnvironment();
+
+        startupLog(env);
+    }
+
+    private static void startupLog(Environment env) {
+        String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
+        String serverPort = env.getProperty("server.port");
+        String contextPath = Optional
+                .ofNullable(env.getProperty("server.servlet.context-path"))
+                .filter(StringUtils::isNotBlank)
+                .orElse("/");
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+
+        log.info(
+                "\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running!:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles()
+        );
+    }
 }
